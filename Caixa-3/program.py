29dcba5d1a959ff7f25b5f7b840637f782a44228
Matheus de Souza Nunes dos Santos <79010085+NunesDevs@users.noreply.github.com>
2022-09-14 11:44:48 -0300

    Update program.py

--- Caixa-3/program.py
+++ Caixa-3/program.py
@@ -1,8 +1,5 @@
-import functions
-functions.inicio()
-valor = input("Valor que deseja sacar: ")
-x = functions.converter(valor)
-z = x; y = [100, 50, 20, 10, 5, 2]
+x = float(input("Valor que deseja sacar: "))
+z = x; y = [100, 50, 20, 10, 5, 2, 1]
 for i in y:
     x = z // i; r = z % i; z = r
     print(x, 'Notas de:', i)
